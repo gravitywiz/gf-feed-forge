8a98a1d29cfd032a594389d3bbba312446fe34f3
Clay Griffiths <clay@claygriffiths.com>
2024-04-29 15:41:06 -0500

    Updated JS to be ES5 compliant so it can be ran through `uglify-js`.

--- assets/js/gf-feed-forge-admin.js
+++ assets/js/gf-feed-forge-admin.js
@@ -1,38 +1,44 @@
 jQuery(function($) {
 	$('#doaction, #doaction2').click(function () {
-		const action = $(this).siblings('select').val();
+		var action = $(this).siblings('select').val();
 		if (action == -1) {
 			return;
 		}
 
-		const defaultModalOptions = '';
-		const entryIds = getLeadIds();
+		var defaultModalOptions = '';
+		var entryIds = getLeadIds();
 
 		if (entryIds.length != 0 && action == 'process_feeds') {
 			resetProcessFeedsUI();
 			tb_show(
-				`<div class='tb-title'><div class='tb-title__logo'></div><div class='tb-title__text'><div class='tb-title__main'>${GFFF_ADMIN.modalHeader}</div><div class='tb-title__sub'>${GFFF_ADMIN.modalDescription}</div></div></div>`,
+				"<div class='tb-title'>" +
+				"<div class='tb-title__logo'></div>" +
+				"<div class='tb-title__text'>" +
+				"<div class='tb-title__main'>" + GFFF_ADMIN.modalHeader + "</div>" +
+				"<div class='tb-title__sub'>" + GFFF_ADMIN.modalDescription + "</div>" +
+				"</div>" +
+				"</div>",
 				'#TB_inline?width=600&height=455&inlineId=feeds_modal_container',
-				'',
+				''
 			);
 			return false;
 		}
 	});
 
 	$('input[name="feed_process"]').on('click', function () {
-		const selectedFeeds = new Array();
+		var selectedFeeds = [];
 
 		$('.gform_feeds:checked').each(function () {
 			selectedFeeds.push($(this).val());
 		});
 
-		const leadIds = getLeadIds();
+		var leadIds = getLeadIds();
 
 		if (selectedFeeds.length <= 0) {
 			displayMessage(
 				GFFF_ADMIN.noSelectedFeedMsg,
 				'error',
-				'#feeds_container',
+				'#feeds_container'
 			);
 			return;
 		}
@@ -52,24 +58,24 @@ jQuery(function($) {
 				$('#feeds_please_wait_container').hide();
 
 				if (response.success) {
-					const count =
+					var count =
 						leadIds == 0
 							? gformVars.countAllEntries
 							: leadIds.length;
 					displayMessage(
 						GFFF_ADMIN.successMsg.replace(
 							'%s',
-							`${count}  ${getPlural(count, GFFF_ADMIN.entryString, GFFF_ADMIN.entriesString)}`,
+							count + "  " + getPlural(count, GFFF_ADMIN.entryString, GFFF_ADMIN.entriesString)
 						),
 						'success',
-						'#entry_list_form',
+						'#entry_list_form'
 					);
 					closeModal(true);
 				} else {
 					closeModal(false);
 					displayMessage(response.data.message, 'error', '#entry_list_form');
 				}
-			},
+			}
 		).fail(function (response) {
 			$('#feeds_please_wait_container').hide();
 			closeModal(false);

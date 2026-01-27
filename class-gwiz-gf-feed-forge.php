902f38b13290fd78e11f57a7af86f5c0c46eae77
Sebastian Pedersen <sebastian@gravitywiz.com>
2026-01-27 12:45:58 -0800

    Added a new `gfff_entry_queued` action fired after an entry is queued for feed processing.

--- class-gwiz-gf-feed-forge.php
+++ class-gwiz-gf-feed-forge.php
@@ -590,12 +590,25 @@ class GWiz_GF_Feed_Forge extends GFAddOn {
 
 				gf_feed_processor()->push_to_queue(
 					[
-						'addon'    => $addon,
+						'addon'    => get_class( $addon ),
 						'feed'     => $feed,
 						'entry_id' => $entry_id,
 						'form_id'  => $feed['form_id'],
 					]
 				);
+
+				/**
+				 * Fires after an entry is queued for feed processing.
+				 *
+				 * @since 1.1.12
+				 *
+				 * @param int         $entry_id The entry ID that was just queued.
+				 * @param array       $entry    The entry object.
+				 * @param array       $feed     The feed being processed.
+				 * @param array       $form     The form object.
+				 * @param GFFeedAddOn $addon    The addon instance.
+				 */
+				do_action( 'gfff_entry_queued', $entry_id, $entry, $feed, $form, $addon );
 			}
 		}
 
